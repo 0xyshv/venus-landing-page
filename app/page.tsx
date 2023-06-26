c0e6c5b84c8ad360776fc944a33f25642b80dded
yshv-2022 <yashasvichaudhary14@gmail.com>
2023-06-26 11:26:35 +0530

    updated venus to main route

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,65 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
 
-export default function Home() {
+import Navbar from "@/components/navbar/page";
+import React from "react";
+import styles from "./venus.module.css";
+import Banner from "@/components/Banner/page";
+import Card from "@/components/Cards/page";
+import Footer from "@/components/Footer/page";
+import AssetRow from "@/components/AssetRow/page";
+
+const cards = [
+  {
+    logo: "a",
+    title: "Decentralized",
+    description: "Access an immutable money market directly on-chain.",
+  },
+  {
+    logo: "b",
+    title: "BEP-20",
+    description: "All Venus Protocol assets are bound by the BEP-20 standard.",
+  },
+  {
+    logo: "c",
+    title: "Scalable",
+    description:
+      "Built on BNB Chain for fast, secure, and low cost transactions.",
+  },
+];
+
+const assets = [
+  { logo: "&#x232c", title: "ABC", description: "vkuwk cwjdcbiw" },
+];
+
+const Venus = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className={styles.mainContainer}>
+      <Navbar />
+      <Banner />
+
+      {/* Market Section */}
+      <div className={styles.assets}>
+        {" "}
+        {assets.map((asset) => (
+          <AssetRow />
+        ))}
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* card section */}
+      <div className={styles.featureCards}>
+        {" "}
+        {cards.map((card) => (
+          <Card
+            logo={card.logo}
+            title={card.title}
+            description={card.description}
+          />
+        ))}
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <Footer />
+    </div>
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Venus;
